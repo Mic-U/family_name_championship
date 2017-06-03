1d4c1867a3e9ca0b851c8071f451c2b99daa01ec
Yuho Miki <yuho.miki@gmail.com>
2017-06-03 18:23:33 +0900

    プロジェクト作成

---
+++ lib/family_name_championship.ex
@@ -0,0 +1,18 @@
+defmodule FamilyNameChampionship do
+  @moduledoc """
+  Documentation for FamilyNameChampionship.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FamilyNameChampionship.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/family_name_championship_test.exs
@@ -0,0 +1,8 @@
+defmodule FamilyNameChampionshipTest do
+  use ExUnit.Case
+  doctest FamilyNameChampionship
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
